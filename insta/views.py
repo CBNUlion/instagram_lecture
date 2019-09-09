fd4fe919d8e9fd8a6f9ab2156b3a88afb05a120c
nohgijin <ynda11111@naver.com>
2019-09-09 17:38:20 +0900

    second commit

--- insta/views.py
+++ insta/views.py
@@ -1,5 +1,25 @@
 from django.shortcuts import render
+from .models import Post
+import operator
 
 # Create your views here.
+
 def main(request):
-    return render(request, 'insta/main.html')
+    posts=Post.objects.all()
+    sort=request.GET.get('sort','')
+
+    if sort=='new':
+        post=Post.objects.all()
+    elif sort=='like':
+        ordered_posts={}
+        post_list=Post.objects.all()
+        for post in post_list:
+            ordered_posts[post]=post.like_count
+        post_list=sorted(ordered_posts.items(),key=operator.itemgetter(1),reverse=True)
+        posts=[]
+        for post in post_list:
+            posts.append(post[0])
+
+    return render(request, 'insta/main.html',{
+        'posts':posts,
+    })
